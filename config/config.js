a5076f2d650da53a1e9be5e85451a9ab775d5240
charanz <61792496+charanzrapeti@users.noreply.github.com>
2022-12-17 21:52:02 +0530

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "CHARANS_secret_key",
+  mongoUri: "mongodb://127.0.0.1:27017/expense"
+}
+
+
+
+// const config = {
+//   env: process.env.NODE_ENV || 'development',
+//   port: process.env.PORT || 3000,
+//   jwtSecret: process.env.JWT_SECRET || "CHARANS_secret_key",
+//   mongoUri: process.env.MONGODB_URI ||
+//     process.env.MONGO_HOST ||
+//     'mongodb://' + (process.env.IP || 'localhost') + ':' +
+//     (process.env.MONGO_PORT || '27017') +
+//     '/mernproject'
+// }
+
+
+export default config
