41182ff254c327937f6d8bc071c634ad8f57cac4
charanz <61792496+charanzrapeti@users.noreply.github.com>
2022-12-18 18:05:09 +0530

    configuration env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "CHARANS_secret_key",
-  mongoUri: "mongodb://127.0.0.1:27017/expense"
-}
+  mongoUri: process.env.DATABASE_URL || "mongodb+srv://charanzrapeti:Hello@123@expense-tracker.ewj5bze.mongodb.net/?retryWrites=true&w=majority",
+};
 
 
 
@@ -17,6 +17,5 @@ const config = {
 //     (process.env.MONGO_PORT || '27017') +
 //     '/mernproject'
 // }
-
 
 export default config
